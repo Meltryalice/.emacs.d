9a2bb31553205eae38c73bdc1d23a4234bc92f33
Meltryalice <alcool815@gmail.com>
2025-07-14 14:11:13 +0800

    add some gc config

--- early-init.el
+++ early-init.el
@@ -1,8 +1,15 @@
+
 ;;; early-init.el --- Early initialization. -*- lexical-binding: t -*-
 
 ;;; Code:
 
 (setq gc-cons-threshold most-positive-fixnum)
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+   (init-gc-cons-threshold (* 128 1024 1024)))
+ (setq gc-cons-threshold init-gc-cons-threshold)
+ (add-hook 'emacs-startup-hook
+      (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
 (setq native-comp-deferred-compilation nil
       native-comp-jit-compilation nil)
